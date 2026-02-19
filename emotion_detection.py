847808ea32b62495b9f35fb4390b8dd778267fac
isurudharmasena <dharmasenaisuru@gmail.com>
2026-02-19 13:40:39 -0500

    task 2

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,22 +1,35 @@
-import requests  # Import the requests library to handle HTTP requests
+import requests 
 import json
 
 def emotion_detection(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' 
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-# Parsing the JSON response from the API
+    
+    # Send a POST request to the API
+    response = requests.post(url, json=myobj, headers=header)  
+    
+    # Parse the JSON response
     formatted_response = json.loads(response.text)
 
-    # Extracting emotions from response
-    anger = formatted_response['emotion']['anger']
-    disgust = formatted_response['emotion']['disgust']
-    fear = formatted_response['emotion']['fear']
-    joy = formatted_response['emotion']['joy']
-    sadness = formatted_response['emotion']['sadness']
+    # Access the 'emotion' dictionary from the first item in the 'emotionPredictions' list
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    # Extract individual emotions
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
 
-    #get dominant emotion
-    dominant_emotion = max(formatted_response['emotion'],key = data.get)
+    # Get dominant emotion using 'emotions.get'
+    dominant_emotion = max(emotions, key=emotions.get)
 
-    return  {'anger': anger, 'disgust':disgust, 'fear':fear,'joy':joy,'sadness':sadness,'dominant_emotion':dominant_emotion} 
+    return {
+        'anger': anger, 
+        'disgust': disgust, 
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
